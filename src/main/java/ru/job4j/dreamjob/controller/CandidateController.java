e3e25b891f5bdf50a3ea5a7a8858d08c65bf7e08
Pavel Litvinenko <korvine1980@yandex.ru>
2025-09-04 14:00:39 +0500

    5. Список кандидатов [#504842]

---
+++ src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
@@ -0,0 +1,26 @@
+package ru.job4j.dreamjob.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.job4j.dreamjob.repository.CandidateRepository;
+
+@Controller
+@RequestMapping("/candidates")
+public class CandidateController {
+
+    private final CandidateRepository candidateRepository;
+
+    @Autowired
+    public CandidateController(CandidateRepository candidateRepository) {
+        this.candidateRepository = candidateRepository;
+    }
+
+    @GetMapping
+    public String getAll(Model model) {
+        model.addAttribute("candidates", candidateRepository.findAll());
+        return "candidates/list";
+    }
+}
